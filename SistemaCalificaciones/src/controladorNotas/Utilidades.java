c2267e5d6589671b22d954753fab4d10f70c04a8
FranciscoChamba <francisco.chamba@unl.edu.ec>
2025-07-24 10:48:11 -0500

    Actualizar Utilidades

--- SistemaCalificaciones/src/controladorNotas/Utilidades.java
+++ SistemaCalificaciones/src/controladorNotas/Utilidades.java
@@ -5,32 +5,43 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.File;
 import java.io.FileReader;
-/** 
+
+/**
  *
  * @author darkj
  */
 public class Utilidades {
+
     private String path = "data";
-    public void save(String text, String name_file) throws IOException{
-        FileWriter file = new FileWriter(path+File.separatorChar+name_file, true);
+
+    public void save(String text, String name_file) throws IOException {
+        FileWriter file = new FileWriter("src" + File.separatorChar + path + File.separatorChar + name_file, true);
         file.write(text);
         file.close();
-        
+
     }
-    public String[][] listAll(String name_file) throws IOException{
-        String [][] data = null;       
+
+    public void actualizar(String text, String name_file) throws IOException {
+        FileWriter file = new FileWriter("src" + File.separatorChar + path + File.separatorChar + name_file);
+        file.write(text);
+        file.close();
+
+    }
+
+    public String[][] listAll(String name_file) throws IOException {
+        String[][] data = null;
         Integer filas = countRegister(name_file);
-        if(filas > 0) {
+        if (filas > 0) {
             Integer col = countColumn(name_file);
-            System.out.println("Columnas "+col);
+            System.out.println("Columnas " + col);
             data = new String[filas][col];
-            FileReader file = new FileReader(path+File.separatorChar+name_file);
+            FileReader file = new FileReader(path + File.separatorChar + name_file);
             BufferedReader br = new BufferedReader(file);
             String linea = br.readLine();
             int fil = 0;
-            while(linea != null) {
+            while (linea != null) {
                 String[] columas = linea.split("\t");
-                for(int j = 0; j < columas.length; j++){
+                for (int j = 0; j < columas.length; j++) {
                     data[fil][j] = columas[j];
                 }
                 fil++;
@@ -39,39 +50,26 @@ public class Utilidades {
             file.close();
             br.close();
         }
-        
+
         System.out.println();
         return data;
     }
-    
-    private int countRegister(String name_file) throws  IOException {
-        FileReader file = new FileReader(path+File.separatorChar+name_file);
-        BufferedReader br = new BufferedReader(file);        
-        int lines = (int)br.lines().count();
+
+    private int countRegister(String name_file) throws IOException {
+        FileReader file = new FileReader(path + File.separatorChar + name_file);
+        BufferedReader br = new BufferedReader(file);
+        int lines = (int) br.lines().count();
         file.close();
         br.close();
         return lines;
     }
-    
-    private int countColumn(String name_file) throws  IOException {
-        FileReader file = new FileReader(path+File.separatorChar+name_file);
-        BufferedReader br = new BufferedReader(file);        
+
+    private int countColumn(String name_file) throws IOException {
+        FileReader file = new FileReader(path + File.separatorChar + name_file);
+        BufferedReader br = new BufferedReader(file);
         String line = br.readLine();
         file.close();
         br.close();
         return line.split("\t").length;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
