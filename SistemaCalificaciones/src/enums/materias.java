6ef0f072cd596c67391a6590e5eb52ea1652c3fe
JohanelENP <johanel.castillo@unl.edu.ec>
2025-07-26 19:33:17 -0500

    avance modulo de materias

---
+++ SistemaCalificaciones/src/enums/materias.java
@@ -0,0 +1,14 @@
+
+package enums;
+
+public enum materias {
+    
+    MATEMATICAS,
+    LENGUA_Y_LITERATURA,
+    CIENCIAS_NATURALES,
+    ESTUDIOS_SOCIALES,
+    INGLES,
+    EDUCACION_FISICA,
+    ARTES,
+    DESARROLLO_HUMANO;  
+}
